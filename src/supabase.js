dede66ba2d718911c6d15df76b545f816eee69b2
fishbowlsoftware <nik@fishbowlllc.com>
2022-10-25 09:52:11 -0400

    minor edits per article review

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tfcejjhqlyrlocdrnfsj.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmY2VqamhxbHlybG9jZHJuZnNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjE0MzgwMTYsImV4cCI6MTk3NzAxNDAxNn0.X8FVYSOKlZoSVhm4-Q-d7FUEsMvQeHlvf2TzObvf2Ho'
-//import.meta.env.VITE_SUPABASE_URL
-//import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
